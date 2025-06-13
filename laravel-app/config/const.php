00672d388ad3ff9a3cfa3c03e22455d65b89cfe8
chiorijjj-san <christianjariol.dev@gmail.com>
2025-06-13 02:27:50 +0000

    add testing

---
+++ laravel-app/config/const.php
@@ -0,0 +1,7 @@
+<?php 
+
+echo "Testing rani if working ba";
+return [
+
+];
+?>
--- laravel-app/resources/views/index.blade.php
+++ laravel-app/resources/views/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Live Chess Game</title>
+    <title>This is a website.</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <style>
         #board { width: 400px; margin: 20px auto; }
