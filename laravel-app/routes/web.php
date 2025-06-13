9cb66d20f248e2a585bf9048e566b61a7274fe26
chiorijjj-san <christianjariol.dev@gmail.com>
2025-06-13 05:38:46 +0000

    fix register1

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -10,4 +10,4 @@ Route::post('/move', [CMainController::class, 'move']);
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
-Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
