93b94d550f4decef0abfcefd88300112600b653d
chiorijjj-san <christianjariol.dev@gmail.com>
2025-06-13 07:08:53 +0000

    fix login controller

--- laravel-app/app/Http/Controllers/Auth/LoginController.php
+++ laravel-app/app/Http/Controllers/Auth/LoginController.php
@@ -1,7 +1,9 @@
 <?php 
+namespace App\Http\Controllers\Auth;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-// use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
