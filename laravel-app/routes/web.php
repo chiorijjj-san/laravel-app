c07422b23acd8efeaab067a6438f337efb0008ed
chiorijjj-san <christianjariol.dev@gmail.com>
2025-06-13 06:58:37 +0000

    add controllers

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 
 Route::get('/', function () {
     return view('index');
