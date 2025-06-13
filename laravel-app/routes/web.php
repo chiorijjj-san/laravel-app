e24234d5a434ce709d232756414025e64922d839
chiorijjj-san <christianjariol.dev@gmail.com>
2025-06-13 05:37:45 +0000

    add register btn

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -9,3 +9,5 @@ Route::get('/', function () {
 Route::post('/move', [CMainController::class, 'move']);
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
+
+Route::post('/register', [RegisterController::class, 'register']);
