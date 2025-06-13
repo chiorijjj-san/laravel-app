db97723f32d71dc1a862c057616900dc8c7eb2d2
chiorijjj-san <christianjariol.dev@gmail.com>
2025-06-13 02:19:20 +0000

    fix view

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('chess');
+    return view('index');
 });
 
 Route::post('/move', [ChessController::class, 'move']);
