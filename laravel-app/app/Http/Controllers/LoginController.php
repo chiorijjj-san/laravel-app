39c8e111d2201124a6ddfcac3f948fcb4d9f77e3
chiorijjj-san <christianjariol.dev@gmail.com>
2025-06-13 07:03:06 +0000

    f

--- laravel-app/app/Http/Controllers/LoginController.php
+++ laravel-app/app/Http/Controllers/LoginController.php
@@ -1,7 +1,7 @@
 <?php 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
