4fdc7a4579fc7aa066016016990d3bdacb560aa9
chiorijjj-san <christianjariol.dev@gmail.com>
2025-06-13 02:18:39 +0000

    reset chess

---
+++ laravel-app/resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Live Chess Game</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <style>
+        #board { width: 400px; margin: 20px auto; }
+    </style>
+</head>
+<body>
+    
+</body>
+<script src="{{ asset('js/script.js') }}"></script>
+</html>
