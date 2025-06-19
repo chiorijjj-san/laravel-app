4e43d84c5b29dc2e96e84ad9104977d32963e723
chiorijjj-san <christianjariol.dev@gmail.com>
2025-06-19 03:18:19 +0000

    deploy ssh add

--- laravel-app/deploy.sh
+++ laravel-app/deploy.sh
@@ -2,10 +2,9 @@
 
 echo "Starting Deployment..."
 
-cd /var/www/laravel-app || { echo "Failed to cd into project directory"; exit 1; }
-
-# Mark repo as safe
-git config --global --add safe.directory /var/www/laravel-app
+# Ensure Git allows working in current directory
+CURRENT_DIR=$(pwd)
+git config --global --add safe.directory "$CURRENT_DIR"
 
 # Pull latest code and capture both stdout and stderr
 echo "Pulling latest code from Git..."
